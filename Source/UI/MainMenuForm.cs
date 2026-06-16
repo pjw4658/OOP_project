91c29434306414942ed8d2d08cd885687136d2fd
pjw4658 <parkjw4658@gmail.com>
2026-06-16 20:40:00 +0900

    게임화면, 메인화면 손상

---
+++ Source/UI/MainMenuForm.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace project_cs.Source.UI
+{
+    public partial class MainMenuForm : Form
+    {
+        public MainMenuForm()
+        {
+            InitializeComponent();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            GameForm gameForm = new GameForm();
+            gameForm.Show();
+            this.Hide();
+        }
+    }
+}
